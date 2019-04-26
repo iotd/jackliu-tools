7bebf5bc38d648b2f8dd45dcd714d3dff866a49b
Jack Liu <stackgo@163.com>
2019-04-26 11:23:08 +0800

    Update windows-batch-rename-files.ps1

--- windows-batch-rename-files.ps1
+++ windows-batch-rename-files.ps1
@@ -1,5 +1,3 @@
-```
-
 #Windows Batch Rename
 #File type: Power Shell
 $Path = 'C:\file path'
@@ -9,4 +7,3 @@ $oldtext = 'old filename text'
 
 ls $Path -Include $Filter -Recurse | ForEach-Object{Rename-Item $_.FullName$_.FullName.Replace($oldtext,$newtext)}
 
-```
